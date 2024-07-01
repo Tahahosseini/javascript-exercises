951bb0593b22916f91b405819fa64abae09f6078
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-01 21:53:12 +0330

    Finish exercise number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let stringToArray = string.split("")
+    let reverseArray = stringToArray.reverse()
+    let reversedString = reverseArray.join("")
 
+    return reversedString
 };
-
 // Do not edit below this line
 module.exports = reverseString;
