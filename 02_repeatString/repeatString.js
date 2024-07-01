b48566b92c7eac6c05f75eb7b2cf3f13d3252bfb
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-01 21:16:12 +0330

    second one done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
+const repeatString = function (string, num) {
+
+    if (num < 0) return 'ERROR'
+    if (num === number) return string.repeat(number)
+    return string.repeat(num)
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
