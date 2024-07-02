17c3c5b87d0cc5714b890b2493f3f139116bb799
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-02 17:21:07 +0330

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...remove) {
+    let newArray = []
+    // go through the array
+    array.forEach((item) => { // when you go through it, do the following
+        if (!remove.includes(item)) // execute the body unless "!" the condition is true
+            newArray.push(item)
+    }
+    )
+    return newArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
